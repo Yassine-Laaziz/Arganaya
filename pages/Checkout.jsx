adece80ddb76e13cdecff4e2de446b8350ff7abd
Yassine <YassineLaaziz0@gmail.com>
2022-11-10 22:16:59 +0100

    Fixing Deployment error

--- pages/Checkout.jsx
+++ pages/Checkout.jsx
@@ -11,68 +11,75 @@ import { urlFor } from "../lib/client"
 const Checkout = () => {
   const { cartItems } = useStateContext()
   const router = useRouter()
-  useEffect(async () => {
-    try {
-      const jwtToken = cookie.parse(document.cookie).jwtToken
-      const req = await axios.post("/api/checkAuthorized", { jwtToken })
-      if (!req.data.verified) router.push("/Verify")
-    } catch (e) {
-      router.push("/Verify")
-    }
+  useEffect(() => {
+    const jwtToken = cookie.parse(document.cookie).jwtToken
+    axios
+      .post("/api/checkAuthorized", { jwtToken })
+      .then((res) => !res.data.verified && router.push("/Verify"))
+      .catch(() => router.push("/Verify"))
   }, [])
 
   return (
     <div className={styles.Checkout}>
       {!cartItems[0] ? (
-        <div className={styles.emptyCart}>
+        <section className={styles.emptyCart}>
           <BsCartXFill className={styles.emptyCartLogo} />
           <p>you currently have nothing on your cart</p>
           <button>
             <Link href="/">Continue shopping?</Link>
           </button>
-        </div>
+        </section>
       ) : (
         <>
-          <div className={styles.dishesContainer}>
+          <section className={styles.dishesContainer}>
             {cartItems.map((item) => (
-              <div className={styles.dishRow}>
+              <div className={styles.dishRow} key={`Checkout${item.name}row`}>
                 <img
+                  key={`Checkout${item.name}image`}
                   src={urlFor(item.images[0])}
                   alt={`Arganaya ${item.name}`}
                 />
-                <div className={styles.info}>
-                  <div className={styles.equations}>
-                    <span>
+                <div className={styles.info} key={`Checkout${item.name}info`}>
+                  <div
+                    className={styles.equations}
+                    key={`Checkout${item.name}equations`}
+                  >
+                    <span key={`Checkout${item.name}equations1`}>
+                      {/* 2 hummus tahini => 2 hummus tahini's' */}
                       {item.quantity} {item.name}
                       {item.quantity > 1 && "s"}
                     </span>
-                    <span>
-                      {item.quantity} * {item.price} ={" "}
-                      {item.quantity * item.price}
+                    <span key={`Checkout${item.name}equations2`}>
+                      {item.quantity} * ${item.price} = {item.quantity * item.price}
                     </span>
                   </div>
-                  <p className={styles.description}>{item.description}</p>
+                  <p
+                    className={styles.description}
+                    key={`Checkout${item.name}description`}
+                  >
+                    {item.description}
+                  </p>
                 </div>
               </div>
             ))}
-          </div>
+          </section>
           <form action="/api/sendOrderEmail" method="post">
             <input
               placeholder="Address Line 1"
               type="text"
-              defaultValue={localStorage.getItem("addressLine1") || ''}
+              defaultValue={localStorage.getItem("addressLine1") || ""}
               required
             />
             <input
               placeholder="Address Line 2"
               type="text"
-              defaultValue={localStorage.getItem("addressLine2") || ''}
+              defaultValue={localStorage.getItem("addressLine2") || ""}
               required
             />
             <input
               placeholder="You phone number (in order to contact you)"
               type="tel"
-              defaultValue={localStorage.getItem("phoneNumber") || ''}
+              defaultValue={localStorage.getItem("phoneNumber") || ""}
               required
             />
             <button type="submit">Order</button>
