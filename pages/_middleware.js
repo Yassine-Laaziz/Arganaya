748c506d61defa5a92994fcfea62ecc6ae8d20fd
Yassine <YassineLaaziz0@gmail.com>
2022-11-11 11:32:49 +0100

    fixed middleware deployment error

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,17 +1,12 @@
 import { verify } from "jsonwebtoken"
 import { NextResponse } from "next/dist/server/web/spec-extension/response"
 
-const middleware = async (req) => {
+const middleware = (req) => {
   const url = req.nextUrl.clone()
   const { cookies } = req
   const { jwtToken } = cookies
-  let verifiedToken
-  if (jwtToken)
-    try {
-      verifiedToken = verify(jwtToken, process.env.JWT_SECRET_KEY)
-    } catch (e) {
-      verifiedToken = null
-    }
+  let verifiedToken = verify(jwtToken, process.env.JWT_SECRET_KEY) || null
+  
 
   if (url.href.includes(`Login`) || url.href.includes(`Signup`)) {
     if (verifiedToken) return NextResponse.redirect(`${url.origin}/`)
@@ -19,9 +14,9 @@ const middleware = async (req) => {
 
   if (url.href.includes(`Checkout`)) {
     if (!verifiedToken) return NextResponse.redirect(`${url.origin}/Login`)
-
-    NextResponse.next()
   }
+  
+  NextResponse.next()
 }
 
 export default middleware
